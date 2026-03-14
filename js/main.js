a00679a9fe5bba9c2f8eaa079c896f8314d86a10
Stuart McCann <81535116+smccsw@users.noreply.github.com>
2026-03-14 23:42:46 +0000

    Styling overhaul started

---
+++ js/main.js
@@ -0,0 +1,16 @@
+(function () {
+  'use strict';
+
+  // Smooth scroll for anchor links (backup for older browsers)
+  document.querySelectorAll('a[href^="#"]').forEach(function (anchor) {
+    anchor.addEventListener('click', function (e) {
+      var id = this.getAttribute('href');
+      if (id === '#') return;
+      var target = document.querySelector(id);
+      if (target) {
+        e.preventDefault();
+        target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    });
+  });
+})();
